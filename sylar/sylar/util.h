31bc714bc75febc6111515d724281cfec2fd8244
fan.jiang <fan.jiang@mobvista.com>
2021-09-03 19:45:07 +0800

    first commit

---
+++ sylar/sylar/util.h
@@ -0,0 +1,61 @@
+/**
+ * @file util.h
+ * @brief 常用的工具函数
+ * @author sylar.yin
+ * @email 564628276@qq.com
+ * @date 2019-05-27
+ * @copyright Copyright (c) 2019年 sylar.yin All rights reserved (www.sylar.top)
+ */
+#ifndef __SYLAR_UTIL_H__
+#define __SYLAR_UTIL_H__
+
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <vector>
+#include <string>
+
+namespace sylar {
+
+/**
+ * @brief 返回当前线程的ID
+ */
+pid_t GetThreadId();
+
+/**
+ * @brief 返回当前协程的ID
+ */
+uint32_t GetFiberId();
+
+/**
+ * @brief 获取当前的调用栈
+ * @param[out] bt 保存调用栈
+ * @param[in] size 最多返回层数
+ * @param[in] skip 跳过栈顶的层数
+ */
+void Backtrace(std::vector<std::string>& bt, int size = 64, int skip = 1);
+
+/**
+ * @brief 获取当前栈信息的字符串
+ * @param[in] size 栈的最大层数
+ * @param[in] skip 跳过栈顶的层数
+ * @param[in] prefix 栈信息前输出的内容
+ */
+std::string BacktraceToString(int size = 64, int skip = 2, const std::string& prefix = "");
+
+/**
+ * @brief 获取当前时间的毫秒
+ */
+uint64_t GetCurrentMS();
+
+/**
+ * @brief 获取当前时间的微秒
+ */
+uint64_t GetCurrentUS();
+
+}
+
+#endif
